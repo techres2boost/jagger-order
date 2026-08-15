c19855cbc422a61af28ed7e666c1844402d14609
techres2boost <tech@res2boost.com>
2026-08-15 16:19:12 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.res2boost.Jagger ",
+appId: "com.res2boost.Jagger",
   appName: "Jagger",
   webDir: ".output/public",
   server: {
